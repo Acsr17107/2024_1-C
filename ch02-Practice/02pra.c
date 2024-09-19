544fce0406a6feb9655d1b26ef67004784468d0f
Neon <sj060700@naver.com>
2024-09-19 18:40:48 +0900

    Delete ch02-Practice directory

--- ch02-Practice/02pra.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("C \nPascal \nJava \nPython \nKotlin\n");
-
-	return 0;
-}
--- ch02-Practice/06pra.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("*\n**\n***\n****\n");
-
-	return 0;
-}
