98f16ea7c99f98b2d1cf0c89f6f9d713f089e4e4
Neon <sj060700@naver.com>
2024-09-29 23:40:40 +0900

    Delete ch03_Practice directory

--- ch03_Practice/01pra.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("%d %d\n", 021, 0x1b);
-
-	return 0;
-}
--- ch03_Practice/06pra.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("%c수업 시간입니다.", '\a');
-
-	return 0;
-}
